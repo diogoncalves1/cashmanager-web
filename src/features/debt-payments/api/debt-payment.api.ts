329cbf39eaf6adfd4c71f2e34068ec003518c4fc
diogogoncalves <dmgdark95@gmail.com>
2026-05-09 16:42:34 +0100

    refactor(debt-payments): move hooks and services to debt-payments feature

---
+++ src/features/debt-payments/api/debt-payment.api.ts
@@ -0,0 +1,49 @@
+import { MyPagination } from "@/features/transactions/types";
+import { DebtPayment } from "@/features/debt-payments/types/index";
+import { Table } from "@tanstack/react-table";
+
+export async function onDeleteDebtPayment(
+  id: string,
+  table?: Table<DebtPayment>,
+  pagination?: MyPagination,
+  mutate?: () => void
+) {
+  const res = await fetch(`/api/debt-payments/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (mutate) await mutate();
+
+  if (data.success) {
+    if (table) {
+      if (table.getRowCount() == 0 && pagination) pagination.pageIndex--;
+    }
+  }
+  return data;
+}
+
+export async function onConfirmDebtPayment(id: string, mutate?: () => void) {
+  try {
+    const res = await fetch(`/api/debt-payments/${id}/confirm`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(data.message);
+
+    if (mutate) await mutate();
+
+    return { message: data.message, success: true };
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message, success: false };
+    } else {
+      return { message: String(err), success: false };
+    }
+  }
+}
