4a764b9f911c991b23585ffe9523c7ea90650e90
diogogoncalves <dmgdark95@gmail.com>
2026-05-10 20:52:23 +0100

    refactor(ui): move all files from components/common/ to components/ui/

---
+++ src/components/ui/ChartTab.tsx
@@ -0,0 +1,299 @@
+import { Currency } from "@/types/currency";
+import { ApexOptions } from "apexcharts";
+import { useTranslations } from "next-intl";
+import React, { useEffect, useState } from "react";
+
+type MonthlyData = {
+  month: Date;
+  revenues: string;
+  expenses: string;
+};
+
+type QuarterlyData = {
+  quarter: string;
+  revenues: number;
+  expenses: number;
+};
+
+type AnnualyData = {
+  year: string;
+  revenues: string;
+  expenses: string;
+};
+
+type ChartDataType = {
+  data: {
+    charts: {
+      monthly: MonthlyData[];
+      quarterly?: QuarterlyData[];
+      annualy?: AnnualyData[];
+    };
+  };
+};
+
+type ChartSeries = {
+  name: string;
+  data: number[];
+};
+
+interface ChartTabProps {
+  data?: ChartDataType;
+  onChangeSeries: (series: ChartSeries[]) => void;
+  onChangeOptions: (prev: ApexOptions) => void;
+  currency?: Currency;
+  height?: number;
+}
+
+const options: ApexOptions = {
+  chart: {
+    type: "bar", // gráfico de colunas
+    height: 310,
+    fontFamily: "Outfit, sans-serif",
+    toolbar: { show: false },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false, // vertical (colunas)
+      columnWidth: "20%", // largura das colunas
+      borderRadius: 4, // cantos arredondados
+    },
+  },
+  colors: ["#12b76a", "#f04438"], // cores das colunas
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ["#12b76a", "#f04438"],
+  },
+  xaxis: {
+    type: "category",
+    categories: [],
+    axisBorder: { show: false },
+    axisTicks: { show: false },
+    tooltip: {
+      enabled: true,
+    },
+  },
+  yaxis: {
+    show: false,
+  },
+  grid: {
+    padding: {
+      left: 10,
+      right: 10,
+    },
+    xaxis: { lines: { show: false } },
+    yaxis: { lines: { show: true } },
+  },
+  tooltip: {
+    enabled: true,
+    shared: true,
+    intersect: false,
+  },
+  legend: { show: false },
+  fill: {
+    type: "solid",
+  },
+};
+
+function ChartTab({ data, onChangeSeries, onChangeOptions, height = 0, currency }: ChartTabProps) {
+  const [chartHeight, setChartHeight] = useState(height);
+
+  const tMonths = useTranslations("MONTHS");
+  const MONTHS: { [key: number]: string } = {
+    0: tMonths("JAN"),
+    1: tMonths("FEB"),
+    2: tMonths("MAR"),
+    3: tMonths("APR"),
+    4: tMonths("MAY"),
+    5: tMonths("JUN"),
+    6: tMonths("JUL"),
+    7: tMonths("AUG"),
+    8: tMonths("SEP"),
+    9: tMonths("OCT"),
+    10: tMonths("NOV"),
+    11: tMonths("DEC"),
+  };
+
+  const t = useTranslations("HOME");
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 1200) setChartHeight(310);
+      else if (window.innerWidth > 600) setChartHeight(310);
+      else setChartHeight(190);
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const [selected, setSelected] = useState<"annualy" | "quarterly" | "monthly">("monthly");
+
+  const getButtonClass = (option: "annualy" | "quarterly" | "monthly") =>
+    selected === option
+      ? "shadow-theme-xs text-gray-900 dark:text-white bg-white dark:bg-gray-800"
+      : "text-gray-500 dark:text-gray-400";
+
+  return (
+    <div className="flex items-center gap-0.5 rounded-lg bg-gray-100 p-0.5 dark:bg-gray-900">
+      <button
+        onClick={function () {
+          const rev = { name: t("REVENUE"), data: [] as number[] };
+          const exp = { name: t("EXPENSE"), data: [] as number[] };
+
+          const months: string[] = [];
+
+          data?.data.charts.monthly.forEach((item) => {
+            months.push(
+              MONTHS[parseInt(String(item.month).split("-")[0]) - 1] +
+                " " +
+                String(item.month).split(" ")[1]
+            );
+
+            rev.data.push(parseFloat(item.revenues));
+            exp.data.push(parseFloat(item.expenses));
+          });
+
+          const series = [rev, exp];
+
+          setSelected("monthly");
+          onChangeSeries(series);
+
+          onChangeOptions({
+            ...options,
+            xaxis: {
+              type: "category",
+              categories: months,
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+            },
+            tooltip: {
+              enabled: true,
+              shared: true,
+              intersect: false,
+              y: {
+                formatter: (value: number) =>
+                  new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: currency?.code,
+                  }).format(value),
+              },
+            },
+          });
+        }}
+        className={`px-3 py-2 font-medium w-full rounded-md text-theme-sm hover:text-gray-900   dark:hover:text-white ${getButtonClass(
+          "monthly"
+        )}`}
+      >
+        {t("MONTHLY")}
+      </button>
+      <button
+        onClick={function () {
+          const rev = { name: t("REVENUE"), data: [] as number[] };
+          const exp = { name: t("EXPENSE"), data: [] as number[] };
+
+          const quartsers: string[] = [];
+
+          data?.data.charts.quarterly?.forEach((item: QuarterlyData) => {
+            quartsers.push(item.quarter);
+
+            rev.data.push(item.revenues);
+            exp.data.push(item.expenses);
+          });
+
+          setSelected("quarterly");
+          onChangeSeries([rev, exp]);
+
+          onChangeOptions({
+            ...options,
+            plotOptions: {
+              bar: {
+                horizontal: false, // vertical (colunas)
+                columnWidth: chartHeight < 380 ? "40%" : "20%", // largura das colunas
+                borderRadius: 4, // cantos arredondados
+              },
+            },
+            xaxis: {
+              type: "category",
+              categories: quartsers,
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+            },
+            tooltip: {
+              enabled: true,
+              shared: true,
+              intersect: false,
+              y: {
+                formatter: (value: number) =>
+                  new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: currency?.code,
+                  }).format(value),
+              },
+            },
+          });
+        }}
+        className={`px-3 py-2 font-medium w-full rounded-md text-theme-sm hover:text-gray-900   dark:hover:text-white ${getButtonClass(
+          "quarterly"
+        )}`}
+      >
+        {t("QUARTERLY")}
+      </button>
+      <button
+        onClick={function () {
+          const rev = { name: t("REVENUE"), data: Array(1).fill(0) };
+          const exp = { name: t("EXPENSE"), data: Array(1).fill(0) };
+
+          const years: string[] = [];
+
+          data?.data.charts.annualy?.forEach((item: AnnualyData, index: number) => {
+            years.push(item.year);
+            rev.data[index] = item.revenues;
+            exp.data[index] = item.expenses;
+          });
+
+          setSelected("annualy");
+          onChangeSeries([rev, exp]);
+
+          onChangeOptions({
+            ...options,
+            plotOptions: {
+              bar: {
+                horizontal: false,
+                columnWidth: chartHeight < 380 ? "15%" : "5%",
+                borderRadius: 4,
+              },
+            },
+            xaxis: {
+              type: "category",
+              categories: years,
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+            },
+            tooltip: {
+              enabled: true,
+              shared: true,
+              intersect: false,
+              y: {
+                formatter: (value: number) =>
+                  new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: currency?.code ?? "USD",
+                  }).format(value),
+              },
+            },
+          });
+        }}
+        className={`px-3 py-2 font-medium w-full rounded-md text-theme-sm hover:text-gray-900 dark:hover:text-white ${getButtonClass(
+          "annualy"
+        )}`}
+      >
+        {t("ANNUALLY")}
+      </button>
+    </div>
+  );
+}
+
+export default ChartTab;
---
+++ src/components/ui/PageBreadCrumb.tsx
@@ -0,0 +1,76 @@
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import React from "react";
+
+interface Breadcrumb {
+  title: string;
+  path?: string;
+}
+
+interface BreadcrumbProps {
+  pageTitle?: string;
+  breadcrumb?: Breadcrumb[];
+}
+
+const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle, breadcrumb }) => {
+  const t = useTranslations("HOME");
+  return (
+    <div className="flex flex-wrap items-center border-b border-gray-100 bg-gray-50 px-5 py-1.5 justify-between gap-3 dark:border-gray-800 dark:bg-white/[0.02]">
+      <h2
+        className="text-sm font-semibold text-gray-600 dark:text-white/90 uppercase"
+        x-text="pageName"
+      >
+        {pageTitle}
+      </h2>
+      <nav>
+        <ol className="flex items-center gap-1">
+          <li>
+            <Link
+              className="inline-flex items-center gap-1.5 text-sm text-gray-500 hover:text-blue-500 dark:text-gray-400"
+              href="/dashboard"
+            >
+              {t("HOME")}
+            </Link>
+          </li>
+          {breadcrumb?.map((item) => (
+            <React.Fragment key={item.title}>
+              <svg
+                className="stroke-current"
+                width="18"
+                height="17"
+                viewBox="0 0 17 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366"
+                  stroke=""
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+
+              <li>
+                {item.path ? (
+                  <Link
+                    className="inline-flex items-center gap-1.5 text-sm text-gray-500 hover:text-blue-500 dark:text-gray-400"
+                    href={item.path}
+                  >
+                    {item.title}
+                  </Link>
+                ) : (
+                  <p className="inline-flex items-center gap-1.5 text-sm text-gray-700 dark:text-gray-300">
+                    {item.title}
+                  </p>
+                )}
+              </li>
+            </React.Fragment>
+          ))}
+        </ol>
+      </nav>
+    </div>
+  );
+};
+
+export default PageBreadcrumb;
